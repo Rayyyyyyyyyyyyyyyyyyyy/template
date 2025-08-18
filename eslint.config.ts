18865c8a9a2267ed12874b653ede3b16d98f19f8
Ray Shao <ray11.shao@china.nttdata.com>
2025-08-18 10:23:24 +0800

    fix: resolve TypeScript config and complete module exports

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,3 @@
 import pluginVue from 'eslint-plugin-vue'
 
-export default {
-  files: ['**/*.{ts,mts,tsx,vue}'],
-  ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
-  ...pluginVue.configs['flat/essential'],
-}
+export default pluginVue.configs['flat/essential']
