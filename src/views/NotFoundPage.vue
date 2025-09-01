1a642dba768142769fb52530b09ab595705d185d
Ray Shao <ray11.shao@china.nttdata.com>
2025-09-01 17:09:12 +0800

    fix: 修復 NotFoundPage 圖片引用問題
    - 移除不存在的 not-found.png 圖片引用
    - 改用 CSS 樣式顯示 404 文字
    - 修復 build:netlify 構建失敗的問題

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -5,9 +5,9 @@ const errorMsg = ref('Oops~ 該頁面不存在唷~')
 </script>
 <template>
   <div class="w-full h-full flex flex-col justify-center items-center gap-4 text-black">
-    <figure class="w-48">
-      <img src="../../assets/images/not-found.png" alt="" onerror="this.style.display='none'" />
-    </figure>
+    <div class="w-48 h-48 flex items-center justify-center bg-gray-100 rounded-lg">
+      <div class="text-6xl text-gray-400">404</div>
+    </div>
     <p class="text-center text-2xl font-bold">{{ errorMsg }}</p>
   </div>
 </template>
